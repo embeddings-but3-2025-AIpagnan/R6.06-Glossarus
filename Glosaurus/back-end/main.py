567885a9516c712389ac4777a18e7d6ae3fc6b30
Vincent Maxence <96425139+Maxvincee@users.noreply.github.com>
2025-12-08 14:47:12 +0100

    fix (windows) : fix POST request

--- Glosaurus/back-end/main.py
+++ Glosaurus/back-end/main.py
@@ -6,19 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-       "http://localhost",
-        "http://localhost:80",
-        "http://localhost:8000",
-        "http://127.0.0.1",
-        "http://localhost:1420",
-        "http://127.0.0.1:1420",
-        "http://127.0.0.1:80",
-        "http://127.0.0.1:8000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "tauri://localhost"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
